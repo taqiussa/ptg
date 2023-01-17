ea37a134f9b9e617569522df9ab4109db842babd
taqiussa <taqiussa@gmail.com>
2023-01-17 15:34:12 +0700

    storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('storage/foto') => storage_path('app/foto'),
+        // public_path('storage/app/foto') => storage_path('app/foto'),
     ],
 
 ];
